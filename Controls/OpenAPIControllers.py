e0b1904674214083c8309668d73e9d81dacc5760
Yashar Honarmandi <yashar.honarmandi@entecon.se>
2024-02-24 01:44:23 +0100

    Implement open APIs

--- Controls/OpenAPIControllers.py
+++ Controls/OpenAPIControllers.py
@@ -1,9 +1,26 @@
+import json
 import tornado.web
+import webappclasses.randomDogWrapper as randomDog
+import webappclasses.randomPublicAPIWrapper as randomPublicAPI
 
 class RandomDogController(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Content-Type", 'application/json')
+    
     def get(self):
-        self.write("This is a random dog")
+        wrapper = randomDog.randomDogWrapper.makeFromConfig("Configs/randomDogConfig.json")
+        
+        res = wrapper.getData()
+        
+        self.write(json.dumps(res.data.__dict__))
 
 class RandomOpenAPIController(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Content-Type", 'application/json')
+    
     def get(self):
-        self.write("This is a random open web API")
+        wrapper = randomPublicAPI.randomPublicAPIWrapper.makeFromConfig("Configs/randomPublicAPIConfig.json")
+
+        res = wrapper.getData()
+        
+        self.write(json.dumps(res.data.__dict__))
